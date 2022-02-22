c9cdc6a568cf0801a7e2094cff02c94eb741f647
Davis <ramelldavis@localhost>
2022-02-22 12:45:39 -0800

    testing

---
+++ Lab02/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("302675880 - This line was added by Ramell Davis.");
+		System.out.println("302799549 - This line was added by Jeremiah Kerzic.");
+		System.out.println("301579876 - This line was added by Michael Penaloza.");
+		System.out.println("301333630 - This line was added by Johnathan Valencia.");
+		System.out.println("302798184 - This line was added by Brian Vela Pasillas.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+///
+	}
+
+}
